1e8974d2510211f9945a608dac428888a496c37f
gavin-wcci <gavinrhensley@gmail.com>
2022-05-09 15:55:40 -0400

    init and classes

---
+++ car-garage-master/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_garage_master
+{
+    internal class Vehicle
+    {
+        public int Speed { get; set; }
+        public int TopSpeed { get; set; }
+
+        public virtual void Accelerate()
+        {
+            Speed++;
+        }
+       
+    }
+}
